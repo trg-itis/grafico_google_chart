f83f65b488f0381a30803230cb422227c83b4acb
Tru ITIS <sluca.truglia@itis.pr.it>
2025-03-06 13:44:48 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 class Grafico {
     constructor(elementId) {
         this.elementId = elementId;
-        this.points = [['X', 'Y']]
+        this.points = [['X', 'Y']];
         google.charts.load('current', {'packages':['corechart']});
     }
 
-    addPoint(x,y){
-        this.points.push([x,y])
+    addPoint(x, y) {
+        this.points.push([x, y]);
     }
 
-    draw(){
+    draw() {
         google.charts.setOnLoadCallback(() => this.drawChart());
     }
 
@@ -18,7 +18,7 @@ class Grafico {
 
         var options = {
             hAxis: {title: 'X', viewWindow: {min: -10, max: 10}},
-            vAxis: {title: 'Y', viewWindow: {min: -10, max:10}},
+            vAxis: {title: 'Y', viewWindow: {min: -10, max: 10}},
             curveType: 'function',
             legend: 'none',
             pointSize: 2
@@ -29,7 +29,7 @@ class Grafico {
     }
 }
 
-class GraficoRetta extends Grafico{
+class GraficoRetta extends Grafico {
     drawFromFormula(a, b, c) {
         this.points = [['X', 'Y']];
         for (let x = -10; x <= 10; x += 0.1) {
@@ -40,8 +40,9 @@ class GraficoRetta extends Grafico{
     }
 }
 
-class GraficoParabola extends Grafico{
+class GraficoParabola extends Grafico {
     drawFromFormula(a, b, c) {
+        this.points = [['X', 'Y']];
         for (let x = -10; x <= 10; x += 1) {
             let y = a * x * x + b * x + c;
             this.addPoint(x, y);
@@ -50,8 +51,9 @@ class GraficoParabola extends Grafico{
     }
 }
 
-class GraficoCirconferenza extends Grafico{
+class GraficoCirconferenza extends Grafico {
     drawFromFormula(a, b, r) {
+        this.points = [['X', 'Y']];
         for (let i = 0; i <= 2 * Math.PI; i += 0.1) {
             let x = a + r * Math.cos(i);
             let y = b + r * Math.sin(i);
@@ -61,11 +63,37 @@ class GraficoCirconferenza extends Grafico{
     }
 }
 
-const myChart = new GraficoCirconferenza('curve_chart');
+let selectedGraph = undefined;
 
-myChart.drawFromFormula(0,0,10)
+function selectGraph(val) {
+    selectedGraph = val.value;
+}
 
 function createGrafico() {
-    console.log(this.value);
-    
+    if (!selectedGraph) {
+        alert("Please select a graph type.");
+        return;
+    }
+
+    let chart;
+    let a = parseFloat(document.getElementById("a").value);
+    let b = parseFloat(document.getElementById("b").value);
+    let c = parseFloat(document.getElementById("c").value);
+
+    switch (selectedGraph) {
+        case "retta":
+            chart = new GraficoRetta('curve_chart');
+            break;
+        case "parabola":
+            chart = new GraficoParabola('curve_chart');
+            break;
+        case "circonferenza":
+            chart = new GraficoCirconferenza('curve_chart');
+            break;
+        default:
+            alert("Invalid graph type selected.");
+            return;
+    }
+
+    chart.drawFromFormula(a, b, c);
 }
